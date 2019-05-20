440be7a063d7be1983753c82bea215f4ca519c91
talypov <anton.t@thewhitelabelagency.com>
2019-05-20 15:09:48 +0300

    initial commit

---
+++ application/core/Router.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace application\core
+
+class Router {
+
+  protected $routes = [];
+  protected $routes = [];
+
+  public function __construct() {
+    echo 'i am class'
+  }
+
+  public function add() {
+
+  }
+
+  public function match() {
+
+  }
+
+  public function run() {
+    echo 'start';
+  }
+}
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'application/lib/Dev.php';
+
+use application\core\Router;
+// use application\lib\Db;
+
+// require 'application/core/Router.php';
+
+spl_autoload_register(function($class) {
+  $path = str_replace('\\', '/', $class.'.php');
+  if (file_exists($path)) {
+    require $path
+  }
+});
+
+session_start();
+
+$router = new Router;
+$router->run();
+// $router = new Db;
