d556f5f79f79175c6c829cd82c43170417878507
talypov <anton.t@thewhitelabelagency.com>
2019-05-21 12:38:31 +0300

    initial commit 2

--- application/core/Router.php
+++ application/core/Router.php
@@ -1,25 +1,45 @@
 <?php
 
-namespace application\core
+namespace application\core;
 
 class Router {
 
   protected $routes = [];
-  protected $routes = [];
+  protected $params = [];
 
   public function __construct() {
-    echo 'i am class'
+    $arr = require 'application/config/routes.php';
+    foreach ($arr as $key => $val) {
+      $this->add($key, $val);
+    }
   }
 
-  public function add() {
-
+  public function add($route, $params) {
+    $route = '#^'.$route.'$#';
+    $this->routes[$route] = $params;
   }
 
   public function match() {
-
+    $url = trim($_SERVER['REQUEST_URI'], '/');
+    foreach ($this->routes as $route => $params) {
+      if (preg_match($route, $url, $matches)) {
+          $this->params = $params;
+          return true;
+      }
+    }
   }
 
   public function run() {
-    echo 'start';
+    if ($this->match()) {
+     $controller = 'application\controllers\\'.ucfirst($this->params['controller']).'Controller.php';
+      if(class_exist($controller)){
+        //
+      } else {
+        echo 'not found: '.$controller;
+      }
+    } else {
+      echo 'router dont find';
+    }
+    // echo 'start';
   }
 }
--- index.php
+++ index.php
@@ -10,7 +10,7 @@ use application\core\Router;
 spl_autoload_register(function($class) {
   $path = str_replace('\\', '/', $class.'.php');
   if (file_exists($path)) {
-    require $path
+    require $path;
   }
 });
 
